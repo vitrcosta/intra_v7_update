736aaae565054bee452630bbfdabdbf5a3034944
criativittá <suporte@criativitta.com.br>
2024-08-30 11:48:53 -0300

    gallery fix v3

--- app/controllers/intranet/veiculos_controller.rb
+++ app/controllers/intranet/veiculos_controller.rb
@@ -1,51 +1,51 @@
 class Intranet::VeiculosController < Intranet::IntranetController
-  before_action :set_intranet_veiculo, only: %i[ show edit update destroy ]
+  before_action :set_veiculo, only: %i[ show edit update destroy ]
 
   # GET /intranet/veiculos or /intranet/veiculos.json
   def index
-    @intranet_veiculos = Veiculo.all
+    @veiculos = Veiculo.all
     respond_to do |format|
       format.html
       format.js
-      format.xml { @intranet_veiculos = Veiculo.where.not(inativo: true) }
-      format.json { render json: @intranet_veiculos = Veiculo.where.not(inativo: true)}
+      format.xml { @veiculos = Veiculo.where.not(inativo: true) }
+      format.json { render json: @veiculos = Veiculo.where.not(inativo: true)}
     end
   end
 
   # GET /intranet/veiculos/1 or /intranet/veiculos/1.json
   def show
-    set_intranet_veiculo()
+    set_veiculo()
     respond_to do |format|
       format.html
       format.js
-      format.xml { @intranet_veiculo }
-      format.json { render json: @intranet_veiculo}
+      format.xml { @veiculo }
+      format.json { render json: @veiculo}
     end
   end
 
   # GET /intranet/veiculos/new
   def new
-    @intranet_veiculo = Veiculo.new
-    @intranet_veiculo.galleries.build
+    @veiculo = Veiculo.new
+    @veiculo.galleries.build
   end
 
   # GET /intranet/veiculos/1/edit
   def edit
-    @intranet_veiculo = Veiculo.find(params[:id])
-    @intranet_veiculo.galleries.build
+    @veiculo = Veiculo.find(params[:id])
+    @veiculo.galleries.build
   end
 
   # POST /intranet/veiculos or /intranet/veiculos.json
   def create
-    @intranet_veiculo = Veiculo.new(intranet_veiculo_params)
+    @veiculo = Veiculo.new(veiculo_params)
 
     respond_to do |format|
-      if @intranet_veiculo.save
-        format.html { redirect_to intranet_veiculo_url(@intranet_veiculo), notice: "Veiculo was successfully created." }
-        format.json { render :show, status: :created, location: @intranet_veiculo }
+      if @veiculo.save
+        format.html { redirect_to intranet_veiculo_url(@veiculo), notice: "Veiculo was successfully created." }
+        format.json { render :show, status: :created, location: @veiculo }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @intranet_veiculo.errors, status: :unprocessable_entity }
+        format.json { render json: @veiculo.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -53,34 +53,34 @@ class Intranet::VeiculosController < Intranet::IntranetController
   # PATCH/PUT /intranet/veiculos/1 or /intranet/veiculos/1.json
   def update
     respond_to do |format|
-      if @intranet_veiculo.update(intranet_veiculo_params)
-        format.html { redirect_to intranet_veiculos_url(), notice: "Veiculo was successfully updated." }
-        format.json { render :show, status: :ok, location: @intranet_veiculo }
+      if @veiculo.update(veiculo_params)
+        format.html { redirect_to veiculos_url(), notice: "Veiculo was successfully updated." }
+        format.json { render :show, status: :ok, location: @veiculo }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @intranet_veiculo.errors, status: :unprocessable_entity }
+        format.json { render json: @veiculo.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /intranet/veiculos/1 or /intranet/veiculos/1.json
   def destroy
-    @intranet_veiculo.destroy!
+    @veiculo.destroy!
 
     respond_to do |format|
-      format.html { redirect_to intranet_veiculos_url, notice: "Veiculo was successfully destroyed." }
+      format.html { redirect_to veiculos_url, notice: "Veiculo was successfully destroyed." }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_intranet_veiculo
-      @intranet_veiculo = Veiculo.find(params[:id])
+    def set_veiculo
+      @veiculo = Veiculo.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
-    def intranet_veiculo_params
-      params.require(:intranet_veiculo).permit(:ano, :foto_principal, :modelo, :preco, :observacao, :descricao, :quilometragem, :cor, :anofab, :combustivel, :procedencia, :inativo, :destaque, :codigo, :versao, :placa, :galleries_attributes => [:image, :id, :_destroy, :position ])
+    def veiculo_params
+      params.require(:veiculo).permit(:ano, :foto_principal, :modelo, :preco, :observacao, :descricao, :quilometragem, :cor, :anofab, :combustivel, :procedencia, :inativo, :destaque, :codigo, :versao, :placa, :galleries_attributes => [:image, :id, :_destroy, :position ])
     end
 end
